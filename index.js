845f8905d590d02e7d5773c9320f17593dbd6f7a
David Tewkesbury <tewkesburyd@hotmail.com>
2022-10-31 12:26:43 -0600

    commit tree 589f2056ce84df311fbf0a07c3a5a38cd64cb947

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if (num <= 400){
+    return 'This one is on me!';
+  } else if (num >= 400 && num < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (num >= 2000 && num <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (true) {
+    case (tip === 'generous'):
+      return 'Thank you so much.';
+      break;
+    case (tip === 'not as generous'):
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+  }
 }
